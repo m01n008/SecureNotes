75ee0f7ce8662e87382e386aca1a0c65bb5f26f1
m01n008 <iamoin008@gmail.com>
2025-04-16 07:25:20 +0500

    Updating LoginViewModel with dispatch function to update mutable loginState according to user intent/action

--- app/src/main/java/com/m01n008/securenotes/presentation/login/LoginViewModel.kt
+++ app/src/main/java/com/m01n008/securenotes/presentation/login/LoginViewModel.kt
@@ -1,6 +1,15 @@
 package com.m01n008.securenotes.presentation.login
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
 class LoginViewModel: ViewModel() {
+    private val _loginState = MutableStateFlow(LoginState())
+    val loginState : StateFlow<LoginState> = _loginState
+
+    fun dispatch(loginIntent: LoginIntent){
+        val newState = loginReducer(loginIntent, _loginState.value)
+        _loginState.value = newState
+    }
 }
